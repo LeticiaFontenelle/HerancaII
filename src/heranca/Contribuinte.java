6f5609d235f1676c65fa69a5caac136f2115ad8c
Leticia Fontenelle <leticiafontenelle@yahoo.com.br>
2022-05-21 16:07:57 -0300

    HerancaII

---
+++ src/heranca/Contribuinte.java
@@ -0,0 +1,23 @@
+package heranca;
+
+public class Contribuinte {
+	
+	private String nome;
+
+    public void setNome(String nome) {
+
+          this.nome=nome;
+
+    }
+
+  public String getNome () {
+
+    return (nome);
+
+  }
+
+}
+
+
+
+
